7020ab1807a80f7712c01c369d35de39b66dd643
TASoft Applications <info@tasoft.ch>
2017-03-07 09:04:07 +0100

    Update KeyPathHelperClass.php

--- src/KeyPathHelperClass.php
+++ src/KeyPathHelperClass.php
@@ -1,8 +1,40 @@
 <?php
+/**
+*	TASoft Key-Value-Coding
+*	@link https://www.tasoft.ch
+*	@copyright 2017 - 2027 (c) TASoft Applications, Th. Abplanalp
+*	@license MIT
+*/
 namespace TASoft\KVC;
 
+/**
+*	The KVC Helper Class is designed to resolve keypaths and trace to objects for you.
+*	Tracing to objects which do not implement the Object\CodingInterface will cause an exception.
+*/
 class KeyPathHelperClass {
 	
+	/**
+	Reads a keyPath and extract the final key and the path to that object.
+	For example a keypath of "customer.address.country" will be resolved to an array of
+	["customer", "address"] and a key named "country".
+	@param keyPath string
+	@param key string* The final key
+	@return array
+	*/
+	public static function parseKeyPath($keyPath, &$key=NULL) {
+		$kp = explode(".", $keyPath);
+		$key = array_pop($kp);
+		return $kp;
+	}
+	
+	/**
+	Traces from a given object by applying a keypath to the target object, extracting the final key as well.
+	It does not return the key value itself. Only the last object in the chain.
+	@param keyPath string The keypath
+	@param object object An object to trace
+	@param key string* the final key
+	@return object
+	*/
 	public static function traceKeyPathWithObject(
 		$keyPath,
 		$object,
@@ -21,11 +53,5 @@ class KeyPathHelperClass {
 				throw new Exception("Key-Value coding does only accept object values. Key `$path` is `$object`", 194);
 		}
 		return $object;
-	}
-	
-	public static function parseKeyPath($keyPath, &$key=NULL) {
-		$kp = explode(".", $keyPath);
-		$key = array_pop($kp);
-		return $kp;
 	}
 }
