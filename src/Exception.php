4fc3e27dd0969b935612aa6461a1e99cd0099ae5
TASoft Applications <info@tasoft.ch>
2017-03-07 09:03:15 +0100

    Update Exception.php

--- src/Exception.php
+++ src/Exception.php
@@ -1,6 +1,14 @@
 <?php
+/**
+*	TASoft Key-Value-Coding
+*	@link https://www.tasoft.ch
+*	@copyright 2017 - 2027 (c) TASoft Applications, Th. Abplanalp
+*	@license MIT
+*/
 namespace TASoft\KVC;
 
+/**
+*	Exception used by the key-value coding library
+*/
 class Exception extends \Exception {
-	
 }
